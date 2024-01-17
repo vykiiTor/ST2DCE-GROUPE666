4b2822989506efebcc8df3d11cd2781841ac94e6
Benjamin Liszewski <benjamin.liszewski@orange.fr>
2024-01-17 14:58:45 +0100

    fix ma merde

--- webapi/main.go
+++ webapi/main.go
@@ -19,7 +19,7 @@ func main() {
 
 func whoAmI(response http.ResponseWriter, r *http.Request) {
 	who := []whoami{
-		whoami{Name: "TEAM 666",
+		whoami{Name: "Efrei Paris",
 			Title: "DevOps and Continous Deployment",
 			State: "FR",
 		},
